a7368653d3ad2f7c64c0f5f2f438de5ba1d5946a
vinayagowda patil <vinayagowda.patil@apdcomms.co.uk>
2019-12-12 22:04:34 +0530

    cleaning

--- ContactWebAPI/App_Start/WebApiConfig.cs
+++ ContactWebAPI/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Http;
+﻿using System.Linq;
+using System.Web.Http;
 
 namespace ContactWebAPI
 {
@@ -16,6 +17,9 @@ namespace ContactWebAPI
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
         }
     }
 }
